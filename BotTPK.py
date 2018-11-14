59dacec85466e1e8ad797ce31d2175ea710e1b8c
Tf CSU <tfcsumail@gmail.com>
2018-11-14 11:23:06 +0500

    next up

--- BotTPK.py
+++ BotTPK.py
@@ -36,6 +36,6 @@ new_offset = None
 
 if __name__ == '__main__':
     for i in range(10):
-        BotTPK.get_last_update()
-        BotTPK.send_message(BotTPK.last_update['message']['chat']['id'],'Hello')
+        last_update = BotTPK.get_last_update()
+        BotTPK.send_message(last_update['message']['chat']['id'],'Hello')
         
